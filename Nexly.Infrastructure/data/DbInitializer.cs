e9fcad8d7adfc5e3564bfb4895f64aa1360517ac
Derrick Xiang <derrick.xiang@gmail.com>
2026-02-16 09:43:29 +0800

    Add DbInitializer to seed user and article data
    This class seeds initial user and news article data into the database.

---
+++ Nexly.Infrastructure/data/DbInitializer.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Identity;
+using Nexly.Domain;
+
+namespace Nexly.Infrastructure.data
+{
+    public class DbInitializer
+    {
+        public static async Task SeedData(NexlyDbContext context, UserManager<User> userManager)
+        {
+            var users = new List<User>
+                {
+                    new() {Id = "bob-id", DisplayName = "Bob", UserName = "bob@test.com", Email = "bob@test.com"},
+                    new() {Id = "tom-id", DisplayName = "Tom", UserName = "tom@test.com", Email = "tom@test.com"},
+                    new() {Id = "jane-id", DisplayName = "Jane", UserName = "jane@test.com", Email = "jane@test.com"}
+                };
+
+            if (!userManager.Users.Any())
+            {
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
+            if (context.NewsArticles.Any()) return;
+
+            var articles = new List<NewsArticle>
+            {
+                new()
+                {
+                    Title = "Test article 1",
+                    SourceName = "NewsAsia",
+                    PublishedAt = DateTime.Now.AddDays(-1),
+                    SourceUrl = "http://some-url",
+                    Summary = "Summary of the article 1",
+                },
+                new()
+                {
+                    Title = "Test article 2",
+                    SourceName = "NewsAsia",
+                    PublishedAt = DateTime.Now.AddDays(-1),
+                    SourceUrl = "http://some-url",
+                    Summary = "Summary of the article 2",
+                },
+                new()
+                {
+                    Title = "Test article 3",
+                    SourceName = "NewsAsia",
+                    PublishedAt = DateTime.Now.AddDays(-1),
+                    SourceUrl = "http://some-url",
+                    Summary = "Summary of the article 3",
+                }
+            };
+
+            context.NewsArticles.AddRange(articles);
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
