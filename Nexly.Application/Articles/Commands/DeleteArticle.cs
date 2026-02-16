43722561cc022da59595bfd143056343f7912d28
Derrick Xiang <derrick.xiang@gmail.com>
2026-02-16 09:56:50 +0800

    Add DeleteArticle command and handler
    Implements the DeleteArticle command to remove articles from the database.

---
+++ Nexly.Application/Articles/Commands/DeleteArticle.cs
@@ -0,0 +1,31 @@
+using MediatR;
+using Nexly.Application.Core;
+
+namespace Nexly.Application.Articles.Commands
+{
+    public class DeleteArticle
+    {
+        public class Command: IRequest<Result<Unit>>
+        {
+            public required string Id { get; set; }
+        }
+
+        public class Handler(NexlyDbContext context) : IRequestHandler<Command, Result<Unit>>
+        {
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var article = await context.NewsArticles.FindAsync([Guid.Parse(request.Id)], cancellationToken);
+
+                if (article == null) return Result<Unit>.Failure("article not found", 404);
+
+                context.Remove(article);
+
+                var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+                if (!result) return Result<Unit>.Failure("Failed to delete the article", 400);
+
+                return Result<Unit>.Success(Unit.Value);
+            }
+        }
+    }
+}
