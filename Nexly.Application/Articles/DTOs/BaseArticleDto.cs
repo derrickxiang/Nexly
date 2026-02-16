5d3afab2cdcad1443f10a31b5a5c25411de9fe82
Derrick Xiang <derrick.xiang@gmail.com>
2026-02-16 14:54:15 +0800

    Add BaseArticleDto class for article data transfer

---
+++ Nexly.Application/Articles/DTOs/BaseArticleDto.cs
@@ -0,0 +1,26 @@
+namespace Nexly.Application.Articles.DTOs
+{
+    public class BaseArticleDto
+    {
+        public string Title { get; set; }
+        public string TitleZh { get; set; }
+
+        public string Content { get; set; }
+
+        public string Summary { get; set; }
+        public string SummaryZh { get; set; }
+
+        public string SourceUrl { get; set; }
+        public string SourceName { get; set; }
+
+        public DateTime PublishedAt { get; set; }
+        public DateTime CollectedAt { get; set; }
+
+        public string Language { get; set; }
+
+        public string Hash { get; set; }
+        public string? ClusterId { get; set; }
+
+        public float QualityScore { get; set; }
+    }
+}
